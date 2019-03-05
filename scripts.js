5418d86b21ad2133e6ec6cdddd797ab6f66782e3
Sergey Glazov <yaglazov@gmail.com>
2019-03-06 01:03:12 +0300

    меняю meta[name=theme-color] в зависимости от активной и запомненой темы

--- scripts.js
+++ scripts.js
@@ -1,27 +1,40 @@
 (function () {
-  var docBody = document.body;
-  var toggle  = document.getElementById('toggle');
+  var docBody         = document.body;
+  var toggle          = document.getElementById('toggle');
+  var metaThemeColor  = document.querySelector("meta[name=theme-color]");
 
-
-  if ( localStorage.getItem('themeType') != null ) {
-    getThemeType      = localStorage.themeType;
-    docBody.className = getThemeType;
-    if ( localStorage.getItem('themeType') == 'dark' ) {
-      toggle.checked = true;
-    }
+  function setMetaThemeColor() {
+    var pageStyle       = getComputedStyle(docBody);
+    var backgroundColor = pageStyle.getPropertyValue('--background-color');
+        metaThemeColor.setAttribute('content', backgroundColor);
   }
 
 
   toggle.addEventListener('change', function () {
+
     if ( docBody.className == '' ) {
       docBody.classList.toggle('dark');
+      setMetaThemeColor();
     } else if ( docBody.className == 'dark' ) {
       docBody.classList.replace('dark', 'light');
+      setMetaThemeColor();
     } else if ( docBody.className == 'light' ) {
       docBody.classList.replace('light', 'dark');
+      setMetaThemeColor();
     }
+
     localStorage.setItem('themeType', docBody.className);
   });
+
+
+  if ( localStorage.getItem('themeType') != null ) {
+    var getThemeType      = localStorage.themeType;
+        docBody.className = getThemeType;
+    setMetaThemeColor();
+    if ( localStorage.getItem('themeType') == 'dark' ) {
+      toggle.checked = true;
+    }
+  }
 
 
   var darkThemeQuery = '(prefers-color-scheme: dark)';
